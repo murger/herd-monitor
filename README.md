971c5d5e2e9aaee3661aea895f4c3bf85461d72b
Gürhan Mermer <gmermer@gmail.com>
2019-06-29 22:10:45 +0300

    initial structure

---
+++ README.md
@@ -0,0 +1,2 @@
+- create-react-app comes with various bells and whistles attached,
+to keep things concise, I've used a custom boilerplate.
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react';
+
+class App extends Component {
+	render () {
+		return (
+			<Fragment>
+				Voila!
+			</Fragment>
+		);
+	}
+}
+
+export default App;
