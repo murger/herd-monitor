ada78940ae17b90c740b17be239ca4ac12b0f92f
Gürhan Mermer <gmermer@gmail.com>
2019-07-02 14:15:00 +0300

    removed scroll tracking

--- src/components/Paginator/Paginator.js
+++ src/components/Paginator/Paginator.js
@@ -1,40 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { DataConsumer } from '../../contexts/DataContext';
 import { Wrapper, Button } from './style';
 
-class Paginator extends Component {
-	state = {
-		hasScrollableArea: true,
-	};
-
-	trackScrollableArea = () => {
-		const { innerHeight } = window;
-		const { scrollTop, offsetHeight } = document.documentElement;
-		const tolerance = 64;
-		const hasScrollableArea = (innerHeight + scrollTop < offsetHeight - tolerance);
-
-		this.setState({ hasScrollableArea });
-	};
-
-	// componentDidMount() {
-	// 	window.addEventListener('scroll', this.trackScrollableArea);
-	// }
-
-	// componentWillUnmount() {
-	// 	window.removeEventListener('scroll', this.trackScrollableArea);
-	// }
-
-	render () {
-		return (
-			<Wrapper>
-				<DataConsumer>
-					{({ loadTransactions }) => (
-						<Button onClick={loadTransactions}>Load more</Button>
-					)}
-				</DataConsumer>
-			</Wrapper>
-		);
-	}
-}
+const Paginator = () => (
+	<Wrapper>
+		<DataConsumer>
+			{({ loadTransactions }) => (
+				<Button onClick={loadTransactions}>Load more</Button>
+			)}
+		</DataConsumer>
+	</Wrapper>
+);
 
 export default Paginator;
